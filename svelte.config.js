521c0133d17027a2f250f32ea0875afff2920c4a
Cyd Stumpel <info@cydstumpel.nl>
2025-02-02 15:46:34 +0100

    kannie typeen

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,11 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		kit: {
-			adapter: adapter({
-				fallback: '404.html'
-			}),
-			paths: {
-				base: 'https://fdnd.github.io/connect-your-tribe-visitekaartje/'
-			}
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: 'https://fdnd.github.io/connect-your-tribe-visitekaartje/'
 		}
+	}
 };
